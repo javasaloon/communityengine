4d26093b5bfdf0faa716ce1cf69035b27506b07b
jacky <javasaloon@gmail.com>
2012-11-25 21:13:10 +0800

    update page.rb: attr_accessible :title, :body, :page_public, :published_as

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,7 @@
 class Page < ActiveRecord::Base
   acts_as_publishable :live, :draft, :members_only, :public
   validates_presence_of :title, :body
+  attr_accessible :title, :body, :page_public, :published_as
   
   def to_param
     id.to_s << "-" << (title ? title.parameterize : '')
